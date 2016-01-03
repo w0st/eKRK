6104019c20eaef9f9b6d232a6ecb0f571192c612
Wojciech Stępniak <wojtek@niom.pl>
2016-01-03 01:51:44 +0100

    Angular: Add scaffold
    Please check README.md file

---
+++ code/AngularApp/app/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc index
+ * @name app
+ * @description
+ * # app
+ *
+ * Main module of the application.
+ */
+angular.module('app', [
+    'ngResource',
+    'ngAria',
+    'ui.bootstrap',
+    'ngCookies',
+    'ngAnimate',
+    'ngTouch',
+    'ngSanitize',
+    'ui.router',
+    'home',
+])
+
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+
+    $locationProvider.hashPrefix('!');
+
+    // This is required for Browser Sync to work poperly
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+    $urlRouterProvider
+        .otherwise('/');
+
+}])
+
+.run(['$rootScope', function ($rootScope) {
+
+    'use strict';
+
+    console.log('AngularJS run() function...');
+
+}]);
---
+++ code/AngularApp/app/modules/home/homeRoute.js
@@ -0,0 +1,20 @@
+'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.route:HomeRoute
+	* @description
+	* # HomeRoute
+	* Route of the app
+	*/
+
+angular.module('app')
+	.config(['$stateProvider', function ($stateProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'app/modules/home/home.html',
+				controller: 'HomeCtrl',
+				controllerAs: 'vm'
+			});
+	}]);
