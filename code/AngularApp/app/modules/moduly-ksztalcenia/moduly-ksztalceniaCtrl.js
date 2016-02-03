d76abd88715c5a2a4b481d0bc08a106efa66b260
Daniel Furgal <furgaldaniel@gmail.com>
2016-02-03 20:37:52 +0100

    Angular: code format

--- code/AngularApp/app/modules/moduly-ksztalcenia/moduly-ksztalceniaCtrl.js
+++ code/AngularApp/app/modules/moduly-ksztalcenia/moduly-ksztalceniaCtrl.js
@@ -3,73 +3,73 @@
 // TODO Dodać tłumaczenia dla tabeli
 // TODO Konfiguracja Datatable
 
-(function() {
+(function () {
 
-	/**
-	* @ngdoc function
-	* @name app.controller:modulyksztalceniaCtrl
-	* @description
-	* # modulyksztalceniaCtrl
-	* Controller of the app
-	*/
-	angular
-		.module('moduly-ksztalcenia')
-		.controller('ModulyKsztalceniaCtrl', ModulyKsztalcenia);
+    /**
+     * @ngdoc function
+     * @name app.controller:modulyksztalceniaCtrl
+     * @description
+     * # modulyksztalceniaCtrl
+     * Controller of the app
+     */
+    angular
+        .module('moduly-ksztalcenia')
+        .controller('ModulyKsztalceniaCtrl', ModulyKsztalcenia);
 
-		ModulyKsztalcenia.$inject = ['$scope', 'ModulyKsztalceniaService'];
+    ModulyKsztalcenia.$inject = ['$scope', 'ModulyKsztalceniaService'];
 
-		/*
-		* recommend
-		* Using function declarations
-		* and bindable members up top.
-		*/
+    /*
+     * recommend
+     * Using function declarations
+     * and bindable members up top.
+     */
 
-		function ModulyKsztalcenia($scope, ModulyKsztalceniaService) {
-            /*jshint validthis: true */
-            var vm = this;
+    function ModulyKsztalcenia($scope, ModulyKsztalceniaService) {
+        /*jshint validthis: true */
+        var vm = this;
 
-            // Moduł wybrany do usuniecia
-            vm.modulToDelete = null;
+        // Moduł wybrany do usuniecia
+        vm.modulToDelete = null;
 
-            // index zaznaczonego tab-panelu z modulem
-            vm.selectedTab = 0;
+        // index zaznaczonego tab-panelu z modulem
+        vm.selectedTab = 0;
 
-            // Pobranie danych dla modulu
-            ModulyKsztalceniaService.getProfileForPK(1).then(function (result) {
-                vm.profile_modulow = result;
-            }, function (reason) {
-                vm.error = reason;
+        // Pobranie danych dla modulu
+        ModulyKsztalceniaService.getProfileForPK(1).then(function (result) {
+            vm.profile_modulow = result;
+        }, function (reason) {
+            vm.error = reason;
+        });
+
+
+        // Ustawianie wybranej zakładki
+        this.setTab = function (tabId) {
+            vm.selectedTab = tabId;
+        };
+
+
+        // Ustawia modul do usuniacia
+        // Po akceptacji usuniecia modulu wywolywana jest funkcja: deleteSelectedModul
+        this.selectModulToDelete = function (modul) {
+            console.log("select modul to delete: ");
+            console.log(modul);
+            vm.modulToDelete = modul;
+        };
+
+
+        // Usuwa wybrany wczesnie modul
+        this.deleteSelectedModul = function () {
+            ModulyKsztalceniaService.deleteModulKsztalcenia(vm.modulToDelete.id);
+            vm.profile_modulow.forEach(function (profile) {
+                console.log(profile);
+                var index = profile.moduly_ksztalcenia.indexOf(vm.modulToDelete);
+                if (index != -1) {
+                    console.log('delete index: ' + index);
+                    profile.moduly_ksztalcenia.splice(index, 1);
+                }
+
             });
+        };
 
-
-            // Ustawianie wybranej zakładki
-            this.setTab = function (tabId) {
-                vm.selectedTab = tabId;
-            };
-
-
-            // Ustawia modul do usuniacia
-            // Po akceptacji usuniecia modulu wywolywana jest funkcja: deleteSelectedModul
-            this.selectModulToDelete = function(modul) {
-                console.log("select modul to delete: ");
-                console.log(modul);
-                vm.modulToDelete = modul;
-            };
-
-
-            // Usuwa wybrany wczesnie modul
-            this.deleteSelectedModul = function () {
-                ModulyKsztalceniaService.deleteModulKsztalcenia(vm.modulToDelete.id);
-                vm.profile_modulow.forEach(function(profile) {
-                    console.log(profile);
-                    var index =profile.moduly_ksztalcenia.indexOf(vm.modulToDelete);
-                    if(index != -1) {
-                        console.log('delete index: '+index);
-                        profile.moduly_ksztalcenia.splice(index, 1);
-                    }
-
-                });
-            };
-
-        }
+    }
 })();
