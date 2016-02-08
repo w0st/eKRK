d74f96e036d422f5113a4cf54ba5f064a83e72d3
Wojciech Stępniak <wojtek@niom.pl>
2016-02-08 15:52:47 +0100

    Angular: Macierz sladowania

--- code/AngularApp/app/modules/macierzsladowania/macierzsladowaniaCtrl.js
+++ code/AngularApp/app/modules/macierzsladowania/macierzsladowaniaCtrl.js
@@ -22,60 +22,88 @@
 		*/
 
 		function MacierzSladowania(MacierzSladowaniaService, $scope) {
+            /*jshint validthis: true */
+            var vm = this;
 
-            $scope.listKierunkoweEfekty = [
-            ];
+            var init = function() {
+                $scope.kierunkoweEfekty= {};
+                $scope.obszaroweEfekty= {};
+                $scope.ms= {};
+                $scope.listKierunkoweEfekty = [];
 
-			/*jshint validthis: true */
-			var vm = this;
-			$scope.kierunkoweEfekty= {};
-			$scope.obszaroweEfekty= {};
-			$scope.ms= {};
+                MacierzSladowaniaService.getKierunkoweEfektyKsztalcenia().then(function(response) {
+                    $scope.kierunkoweEfekty = response;
+                    $scope.listKierunkoweEfekty = response;
+                    angular.forEach($scope.listKierunkoweEfekty, function(value){
+                        value['drag']= true;
+                    });
+                    console.log("kierunkowe efekty ksztalcenia = ", response)
+                }, function(reason) {
+                    console.log("Nie udalo sie pobrac kierunkowych efektow ksztalcenia");
+                });
 
-            MacierzSladowaniaService.getKierunkoweEfektyKsztalcenia().then(function(response) {
-                 $scope.kierunkoweEfekty = response;
-                 $scope.listKierunkoweEfekty = response;
-                 angular.forEach($scope.listKierunkoweEfekty, function(value){
-                   value['drag']= true;
-                 });
-                console.log("kierunkowe efekty ksztalcenia = ", response)
-            }, function(reason) {
-                console.log("Nie udalo sie pobrac kierunkowych efektow ksztalcenia");
-            });
-
-             MacierzSladowaniaService.getObszaroweEfektyKsztalcenia().then(function(response) {
-                            vm.macierz = response;
-                            $scope.obszaroweEfekty=response;
-                            angular.forEach($scope.obszaroweEfekty, function(efektObszarowy){
-                                angular.forEach(efektObszarowy.kierunkowe_efekty_ksztalcenia, function(efektKsztalcenia){
-                                   // var i = parseInt(efektKsztalcenia.id,10)-1;
-                                    efektKsztalcenia.drag = true;
-                                    //console.log($scope.kierunkoweEfekty[i]);
-                                    //efektKsztalcenia["kod"] = $scope.kierunkoweEfekty[i];
-                                   // efektKsztalcenia["allInf"] = $scope.kierunkoweEfekty[i];
-                                });
-                            });
-
-                        }, function(reason) {
-                            console.log("Nie udalo sie pobrac macierzy");
+                MacierzSladowaniaService.getObszaroweEfektyKsztalcenia().then(function(response) {
+                    vm.macierz = response;
+                    $scope.obszaroweEfekty=response;
+                    angular.forEach($scope.obszaroweEfekty, function(efektObszarowy){
+                        angular.forEach(efektObszarowy.kierunkowe_efekty_ksztalcenia, function(efektKsztalcenia){
+                            // var i = parseInt(efektKsztalcenia.id,10)-1;
+                            efektKsztalcenia.drag = true;
+                            //console.log($scope.kierunkoweEfekty[i]);
+                            //efektKsztalcenia["kod"] = $scope.kierunkoweEfekty[i];
+                            // efektKsztalcenia["allInf"] = $scope.kierunkoweEfekty[i];
                         });
+                    });
 
-            MacierzSladowaniaService.set([
-                    {
-                        "kierunkowe_efekty_ksztalcenia":
-                            [
-                                {
-                                    "id": 1
-                                }
-                            ],
-                        "id": 2
+                }, function(reason) {
+                    console.log("Nie udalo sie pobrac macierzy");
+                });
+            };
+
+            init();
+
+            $scope.canDrop = function(obszarowyEfekt) {
+                return {
+                    accept: function (dragEl) {
+                        console.log('obszarowy efekt =', obszarowyEfekt);
+                        console.log('kierunkowy efekt =', dragEl);
+                        return !_.some(obszarowyEfekt.kierunkowe_efekty_ksztalcenia,  function(kek) {
+                            return dragEl[0].innerHTML.contains(kek.kod);
+                        });
                     }
-            ]).then(function(response) {
-                console.log("Udalo sie zaktualizowac macierz sladowania");
-            }, function(reason) {
-                console.log("Wystapil blad podczas proby aktualizacji macierzy sladowania");
-            });
+                };
+            };
 
+            $scope.selectEfekt = function(efekt, obszarKsztalcenia) {
+                $scope.selectedEfekt = efekt; // copy ref
+                $scope.selectedObszarKsztalcenia = obszarKsztalcenia; // copy ref
+            };
+
+            $scope.deleteEfekt = function() {
+                $scope.selectedObszarKsztalcenia.kierunkowe_efekty_ksztalcenia =
+                    _.reject($scope.selectedObszarKsztalcenia.kierunkowe_efekty_ksztalcenia, $scope.selectedEfekt)
+            };
+
+            $scope.save = function() {
+                var result = [];
+                angular.forEach($scope.obszaroweEfekty, function(obszarowyEfekt) {
+                    result.push({
+                        id: obszarowyEfekt.id,
+                        kierunkowe_efekty_ksztalcenia: _.map(obszarowyEfekt.kierunkowe_efekty_ksztalcenia, function(efekt) {
+                            return {id: efekt.id}
+                        })
+                    });
+                });
+                console.log('result  = ', result);
+                MacierzSladowaniaService.set(result).then(function(response) {
+                    console.log("Udalo sie zaktualizowac macierz sladowania");
+                }, function(reason) {
+                    console.log("Wystapil blad podczas proby aktualizacji macierzy sladowania");
+                });
+            };
+
+            $scope.cancel = function() {
+                init();
+            };
 		}
-
 })();
